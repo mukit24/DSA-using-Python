d7aad91407ee87ba7f8f22ee6f8f94cc0dad23f4
mukit24 <mukithasanpranto4@gmail.com>
2024-12-09 17:18:08 +0600

    sliding window

--- Sliding Window/basic.py
+++ Sliding Window/basic.py
@@ -6,13 +6,17 @@
 
 # Maximum Sum Subarray of Size k
 def max_sum_subarray(arr, k):
-    max_sum = window_sum = sum(arr[:k])
+    max_sum = float('-inf')
+    current_sum = 0
 
-    for i in range(k, len(arr)):
-        window_sum = window_sum - arr[i-k] + arr[i]
-        max_sum = max(max_sum, window_sum)
+    for i in range(len(arr)):
+        current_sum += arr[i]
+        if i >= k - 1:
+            max_sum = max(max_sum, current_sum)
+            current_sum -= arr[i-(k-1)] #remove sum from window
     return max_sum
 
 test_case_1 = [-2, 1, 5, 1, 3, 2]
 k = 3
 print(max_sum_subarray(test_case_1, k))
+# ** This can solve also with prefix sums(Think)
